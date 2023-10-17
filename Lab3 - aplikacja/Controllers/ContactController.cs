432234ac49029c6b9a9b699e55d6b47a1f48b094
Jakub Faltyn <86327309+JakubFaltyn@users.noreply.github.com>
2023-10-17 15:03:37 +0200

    Changes made on lessonh #

---
+++ Lab3 - aplikacja/Controllers/ContactController.cs
@@ -0,0 +1,73 @@
+﻿using Lab3___aplikacja.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Lab3___aplikacja.Controllers
+{
+    public class ContactController : Controller
+    {
+        static Dictionary<int, Contact> _contacts = new Dictionary<int, Contact>();
+        
+        public IActionResult Index()
+        {
+            return View(_contacts);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Contact model)
+        {
+            if(ModelState.IsValid)
+            {
+                    int id = _contacts.Keys.Count != 0 ? _contacts.Keys.Max() : 0;
+                    model.Id = id + 1;
+                    _contacts[model.Id] = model;
+
+                    return RedirectToAction("Index");
+                }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Update()
+        {
+            if (_contacts.Keys.Contains(id))
+            {
+                return View(_contacts[id]);
+            }
+            else
+            {
+                return NotFound();
+            };
+        }
+
+        [HttpPost]
+        public IActionResult Update(Contact model)
+        {
+            if (ModelState.IsValid)
+            {
+                _contacts[model, Index] = model;
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+        [HttpGet]
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
---
+++ Lab3 - aplikacja/Models/Contact.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab3___aplikacja.Models
+{
+    public class Contact
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Wpisz imię")]
+        [StringLength(maximumLength: 100, ErrorMessage = "Zbyt długie imię")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Wpisz adres email")]
+        [EmailAddress(ErrorMessage = "Wpisano niepoprawny email")]
+        public string Email { get; set; }
+        [Phone(ErrorMessage = "Wpisano niepoprawny nr telefonu")]
+        public string Phone { get; set; }
+
+        public DateTime Birth { get; set; }
+    }
+}
